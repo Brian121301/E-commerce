4373a53e826f4d7c4686c6582a561c83854108ed
Ini <inidavies24@gmail.com>
2024-02-09 14:56:21 -0600

    testing out paypal code

---
+++ shopping cart/checkout.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>checkout</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <h1>Welcome to my E-commerce Website</h1>
+    <?php echo 'This is php code'; ?>
+</body>
+</html>
---
+++ shopping cart/shopping_cart.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>cart</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <h1>Welcome to my E-commerce Website</h1>
+    <?php echo 'This iiiiis php code'; ?>
+</body>
+</html>

410be98e172c63e4277d6bbcb0a5f1de8a329f08
Ini <inidavies24@gmail.com>
2024-02-19 19:48:44 -0600

    testing

--- shopping cart/cancel.php
+++
@@ -1,9 +0,0 @@
-<?php
-paypal.Buttons({
-  onCancel: function (data) 
-  {
-    // Show a cancel page, or return to cart
-    window.location.href = './payment_form.php';
-  }
-}).render('#paypal-button-container');
-?>

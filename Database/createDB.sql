8ab62bccee8b751445fd96940bb7332587eff160
Brian121301 <briannajera3@gmail.com>
2024-03-20 17:32:44 -0500

    merge

--- Database/createDB.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    is_verified BOOLEAN NOT NULL DEFAULT FALSE,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);

a6667513530042d03e3ab8d748b23cbe9400de42
Inioluwa Davies <inidavies24@gmail.com>
2024-03-19 23:38:25 -0500

    hosted version 2 with images

--- cart.php
+++ cart.php
@@ -1,3 +1,7 @@
+<?php
+// Include the configuration file  
+require_once 'config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,7 +43,9 @@
 
         <p>Your shopping cart is empty. Start adding some creative products!</p>
         <!-- You can add links to the product listing  -->
-        <a href="checkout.php">Proceed to Checkout</a>
+        <div id="payment-box">
+            <div id="paypal-button-container"></div>
+        </div>
     </section>
 
     <footer>
@@ -76,6 +82,77 @@
             addToCart("Product A", 20.00);
             addToCart("Product B", 15.00);
         });
+    </script>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=<?php echo PAYPAL_SANDBOX ? PAYPAL_SANDBOX_CLIENT_ID : PAYPAL_PROD_CLIENT_ID; ?>&currency=<?php echo $currency; ?>"></script>
+
+    <script>
+        paypal.Buttons({
+            // Sets up the transaction when a payment button is clicked
+            createOrder: (data, actions) => {
+                return actions.order.create({
+                    "purchase_units": [{
+                        "custom_id": "<?php echo $itemNumber; ?>",
+                        "description": "<?php echo $itemName; ?>",
+                        "amount": {
+                            "currency_code": "<?php echo $currency; ?>",
+                            "value": <?php echo $itemPrice; ?>,
+                            "breakdown": {
+                                "item_total": {
+                                    "currency_code": "<?php echo $currency; ?>",
+                                    "value": <?php echo $itemPrice; ?>
+                                }
+                            }
+                        },
+                        "items": [{
+                            "name": "<?php echo $itemName; ?>",
+                            "description": "<?php echo $itemName; ?>",
+                            "unit_amount": {
+                                "currency_code": "<?php echo $currency; ?>",
+                                "value": <?php echo $itemPrice; ?>
+                            },
+                            "quantity": "1",
+                            "category": "DIGITAL_GOODS"
+                        }, ]
+                    }]
+                });
+            },
+            // Finalize the transaction after payer approval
+            onApprove: (data, actions) => {
+                return actions.order.capture().then(function(orderData) {
+                    setProcessing(true);
+
+                    var postData = {
+                        paypal_order_check: 1,
+                        order_id: orderData.id
+                    };
+                    fetch('paypal_checkout_validate.php', {
+                            method: 'POST',
+                            headers: {
+                                'Accept': 'application/json'
+                            },
+                            body: encodeFormData(postData)
+                        })
+                        .then((response) => response.json())
+                        .then((result) => {
+                            if (result.status == 1) {
+                                window.location.href = "payment-status.php?checkout_ref_id=" + result.ref_id;
+                            } else {
+                                const messageContainer = document.querySelector("#paymentResponse");
+                                messageContainer.classList.remove("hidden");
+                                messageContainer.textContent = result.msg;
+
+                                setTimeout(function() {
+                                    messageContainer.classList.add("hidden");
+                                    messageText.textContent = "";
+                                }, 5000);
+                            }
+                            setProcessing(false);
+                        })
+                        .catch(error => console.log(error));
+                });
+            }
+        }).render('#paypal-button-container');
     </script>
 </body>
 
--- gallery.php
+++ gallery.php
@@ -25,24 +25,24 @@
 
     <section>
         <div class="gallery">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\0AD7B0ED-E91F-486E-993D-B60260A04AE9.jpg" alt="Gallery Image 1">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\4ADC37B2-C8DB-4266-95E4-372419EA48D2.jpg" alt="Gallery Image 2">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\7A3E579E-F50C-49CC-81AF-109BC2C5407E.jpg" alt="Gallery Image 3">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\37C09AFD-DFAD-44D5-A389-78F8E1839C26.jpg" alt="Gallery Image 4">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\96CD0DE9-7D55-4026-8015-0FA996470120.jpg" alt="Gallery Image 5">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\A5191EC3-8F3F-4B88-8263-6B4BD2062909.jpg" alt="Gallery Image 6">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_1154.jpg" alt="Gallery Image 7">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_1581.jpg" alt="Gallery Image 8">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_1672.jpg" alt="Gallery Image 9">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_2626.jpg" alt="Gallery Image 10">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_2872.jpg" alt="Gallery Image 11">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_3568.jpg" alt="Gallery Image 12">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_4514.jpg" alt="Gallery Image 13">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_4662.jpg" alt="Gallery Image 14">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_5150.jpg" alt="Gallery Image 15">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_5230.jpg" alt="Gallery Image 16">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_5245.jpg" alt="Gallery Image 17">
-            <img src="C:\Users\palme\OneDrive\Desktop\Mp.html\IMG_5286.jpg" alt="Gallery Image 18">
+            <img src="./assets/images/0AD7B0ED-E91F-486E-993D-B60260A04AE9.jpg" alt="Gallery Image 1">
+            <img src="./assets/images/4ADC37B2-C8DB-4266-95E4-372419EA48D2.jpg" alt="Gallery Image 2">
+            <img src="./assets/images/7A3E579E-F50C-49CC-81AF-109BC2C5407E.jpg" alt="Gallery Image 3">
+            <img src="./assets/images/37C09AFD-DFAD-44D5-A389-78F8E1839C26.jpg" alt="Gallery Image 4">
+            <img src="./assets/images/96CD0DE9-7D55-4026-8015-0FA996470120.jpg" alt="Gallery Image 5">
+            <img src="./assets/images/A5191EC3-8F3F-4B88-8263-6B4BD2062909.jpg" alt="Gallery Image 6">
+            <img src="./assets/images/IMG_1154.jpg" alt="Gallery Image 7">
+            <img src="./assets/images/IMG_1581.jpg" alt="Gallery Image 8">
+            <img src="./assets/images/IMG_1672.jpg" alt="Gallery Image 9">
+            <img src="./assets/images/IMG_2626.jpg" alt="Gallery Image 10">
+            <img src="./assets/images/IMG_2872.jpg" alt="Gallery Image 11">
+            <img src="./assets/images/IMG_3568.jpg" alt="Gallery Image 12">
+            <img src="./assets/images/IMG_4514.jpg" alt="Gallery Image 13">
+            <img src="./assets/images/IMG_4662.jpg" alt="Gallery Image 14">
+            <img src="./assets/images/IMG_5150.jpg" alt="Gallery Image 15">
+            <img src="./assets/images/IMG_5230.jpg" alt="Gallery Image 16">
+            <img src="./assets/images/IMG_5245.jpg" alt="Gallery Image 17">
+            <img src="./assets/images/IMG_5286.jpg" alt="Gallery Image 18">
         </div>
     </section>
 

3aea396bf3e984dea651662e370c6bdcdf3cc64b
Brian121301 <briannajera3@gmail.com>
2024-03-28 15:21:42 -0500

    testing registration page

--- Config.js
+++ Config.js
@@ -1,13 +1,9 @@
 const config = {
-  cognito: {
-    region: 'us-east-2', // e.g., 'us-east-1'
-    cognito: {
-      userPoolId: 'us-east-2_UIovAgpgj',
-      userPoolWebClientId: '4hbcdt8enhb8oud6tbd3ph6b58',
-    },
-    api: {
-      invokeUrl: 'https://aielkezf1k.execute-api.us-east-2.amazonaws.com/Dev',
-  },
+  region: 'us-east-2', // AWS region
+  userPoolId: 'us-east-2_UIovAgpgj', // User Pool ID
+  clientId: '4hbcdt8enhb8oud6tbd3ph6b58', // App Client ID
+  invokeUrl: 'https://aielkezf1k.execute-api.us-east-2.amazonaws.com/Dev', // API Gateway invoke URL
 };
 
 module.exports = config;
+
